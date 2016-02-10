c5029269f3050fe59f5e6b1b937cd4d140274534
Nathan M <nathanmaynes@gmail.com>
2016-02-10 16:10:22 -0500

    Updated MainClass
    Class updated to enable JavaExec to run without arguments from build.gradle.

--- child2Java6/src/main/java/com/test/MainClass.java
+++ child2Java6/src/main/java/com/test/MainClass.java
@@ -1,13 +1,15 @@
 package com.test;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.stream.Stream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by NWM on 10/02/2016.
  */
-class Person {
+public class MainClass {
+
+    class Person {
         String name;
         int age;
 
@@ -18,7 +20,7 @@ class Person {
 
         @Override
         public String toString() {
-            return name;
+            return name + ": " + age;
         }
     }
 
@@ -26,7 +28,7 @@ class Person {
             Arrays.asList(
                     new Person("Nathan", 36),
                     new Person("Harry", 26),
-                    new Person("Isabell", 56),
+                    new Person("Isabelle", 56),
                     new Person("Sally", 76),
                     new Person("Pauline", 76),
                     new Person("Peter", 16));
@@ -34,10 +36,15 @@ class Person {
     List<Person> filtered =
             persons
                     .stream()
-                    //the lambda expression below will fail and Gradle will act appropriately. 
+                    //the lambda expression below will fail if Java 6 is set in build.gradle and Gradle will act appropriately.
                     .filter(p -> p.name.startsWith("P"))
                     .collect(Collectors.toList());
 
-           System.out.println(filtered);
+    public void run(){
+        System.out.println(filtered);
+    }
 
+    public static void main(String[] args){
+        new MainClass().run();
+    }
 }
