defa112d2e9f090bdef7a9162c741c8f16adc547
Amine Achergui <amine.achergui@aurea.com>
2016-02-10 17:30:50 +0000

    first commit

---
+++ child1Java8/src/main/java/com/test/MainClass.java
@@ -0,0 +1,14 @@
+package com.test;
+
+import java.util.stream.Stream;
+
+/**
+ * Created by mbp on 10/02/2016.
+ */
+public class MainClass {
+
+    public static void main(String... args) {
+        Stream<String> stream = null;
+
+    }
+}
---
+++ child2Java6/src/main/java/com/test/MainClass.java
@@ -0,0 +1,18 @@
+package com.test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+/**
+ * Created by mbp on 10/02/2016.
+ */
+public class MainClass {
+
+    public static void main(String... args) {
+        Stream<String> stream = null;
+
+        Path path = Paths.get("paths");
+
+    }
+}
